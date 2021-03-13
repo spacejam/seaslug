de736b920741dd3b2a2f72c44936bb9f445421c4
Tyler Neely <t@jujit.su>
2021-03-13 18:20:43 +0100

    Initial check-in of some code

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! puke
+//!
+//! a **very** early-stage language with a focus on:
+//!
+//! * the terseness of erlang/prolog
+//! * first-class fault injection, simulation and model-based testing capabilities
+//! * built-in data structures
+//! * high quality scheduling built from the beginning to play well with io_uring and iocp
